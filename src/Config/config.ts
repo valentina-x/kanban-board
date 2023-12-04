74902016ffeee36d7beb237488361105c8224b14
Valentina <xzxz100.95@mail.ru>
2023-12-04 09:40:13 +0200

    first commit

--- src/Config/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-	appid: '7493308f6a79519e4ebe096d31b8f739',
-	type: 'weather',
-	lang: 'ru',
-	units: 'metric',
-}
-
-export default config;
--- src/Middleware/api.ts
+++
@@ -1,9 +0,0 @@
-import config from '../Config/config';
-
-const API_URL = `http://api.openweathermap.org/data/2.5/`;
-
-const API_URL_IMG = 'https://openweathermap.org/img/w/';
-
-export {API_URL_IMG};
-export default API_URL;
-
